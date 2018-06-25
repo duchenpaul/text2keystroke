8cbaa56bd5b2d3cf9b73c95c826ad1958e338b2c
Chenny Du <chdu@merkleinc.com>
2018-06-26 00:06:38 +0800

    Auto compile and clean trash

--- compile.bat
+++ compile.bat
@@ -1,1 +1,9 @@
-pyinstaller --onefile ./text2keystroke.py 
+SET appName=text2keystroke
+pyinstaller --onefile .\%appName%.py 
+move dist\%appName%.exe %cd%
+
+del /s /f /q  text2keystroke.spec
+
+REM walkaround RD bug
+timeout 1
+RD /S /Q   dist\ build\
